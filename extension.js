ea60c6f22f77c4af55caee7c6c6ed75400c2e147
gabgds <gabriellesantos.gds2001@gmail.com>
2026-03-19 22:56:13 -0300

    add snoopy commit/push

--- extension.js
+++ extension.js
@@ -1,32 +1,32 @@
 const vscode = require('vscode');
 
 class SnoopyViewProvider {
-    constructor(extensionUri) 
-    {
+    constructor(extensionUri) {
         this._extensionUri = extensionUri;
+        this._views = [];
     }
 
-    resolveWebviewView (webviewView)
-    {
-        this._view = webviewView;
+    resolveWebviewView(webviewView) {
+        this._views.push(webviewView);
+
         webviewView.webview.options = {
             enableScripts: true,
             localResourceRoots: [this._extensionUri]
         };
-        webviewView.webview.html = this._getHtmlContent(webviewView);
 
-        webviewView.webview.onDidReceiveMessage(message => {
-            if (message.command === 'alert') {
-                vscode.window.showInformationMessage(message.text);
-            }
+        webviewView.webview.html = this._getHtmlContent(webviewView.webview);
+
+        webviewView.onDidDispose(() => {
+            this._views = this._views.filter(v => v !== webviewView);
         });
     }
 
-    _getHtmlContent(webview) 
-    {
-        const sleepUri = webview.webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-sleeping.gif'));
-        const typeUri = webview.webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-typing.gif'));
-        const laughUri = webview.webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-laugh.gif'));
+    _getHtmlContent(webview) {
+        const sleepUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-sleeping.gif'));
+        const typeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-typing.gif'));
+        const laughUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-laugh.gif'));
+        const okUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'snoopy-ok.gif'));
+
         return `<!DOCTYPE html>
         <html lang="en">
         <head>
@@ -34,43 +34,37 @@ class SnoopyViewProvider {
             <meta charset="UTF-8">
             <style>
                 #snoopy-img {
-                max-width: 100%;
-                height: auto;
-                display: block;
-                margin: 0 auto;
+                    max-width: 100%;
+                    height: auto;
+                    display: block;
+                    margin: 0 auto;
                 }
-                body { font-family: sans-serif; text-align: center; padding: 20px; }
-                #status { color: #555; }
+                body { font-family: sans-serif; text-align: center; padding: 20px; overflow: hidden; }
             </style>
         </head>
         <body>
             <img id="snoopy-img" src="${sleepUri}" alt="Snoopy GIF">
-
             <script>
-                const vscode = acquireVsCodeApi();
-                const statusElement = document.getElementById('status');
                 const imgElement = document.getElementById('snoopy-img');
                 let timer;
 
                 window.addEventListener('message', event => {
                     const message = event.data;
-
                     switch (message.command) {
                         case 'type':
                             imgElement.src = '${typeUri}';
                             clearTimeout(timer);
-
-                            timer = setTimeout(() => {
-                                imgElement.src = '${sleepUri}';
-                            }, 2080);
+                            timer = setTimeout(() => { imgElement.src = '${sleepUri}'; }, 2080);
                             break;
                         case 'error':
                             imgElement.src = '${laughUri}';
                             clearTimeout(timer);
-
-                            timer = setTimeout(() => {
-                                imgElement.src = '${sleepUri}';
-                            }, 3000);
+                            timer = setTimeout(() => { imgElement.src = '${sleepUri}'; }, 3000);
+                            break;
+                        case 'success':
+                            imgElement.src = '${okUri}';
+                            clearTimeout(timer);
+                            timer = setTimeout(() => { imgElement.src = '${sleepUri}'; }, 3000);
                             break;
                     }
                 });
@@ -80,38 +74,79 @@ class SnoopyViewProvider {
     }
 
     handleType() {
-        if (this._view) {
-            this._view.webview.postMessage({command: 'type'});
-        }
+        this.sendMessage({ command: 'type' });
+    }
+
+    sendMessage(message) {
+        this._views.forEach(view => {
+            view.webview.postMessage(message);
+        });
     }
 }
 
-function activate (context)
-{
+async function activate(context) {
     const provider = new SnoopyViewProvider(context.extensionUri);
+
     context.subscriptions.push(
-        vscode.window.registerWebviewViewProvider('snoopy-view', provider)
+        vscode.window.registerWebviewViewProvider('snoopy-view-explorer', provider)
     );
 
-    let disposable = vscode.workspace.onDidChangeTextDocument(event => {
-        console.log('Document changed:', event.document.fileName);
-        console.log('Changes:', event.contentChanges);
-        provider.handleType();
-    });
-    context.subscriptions.push(disposable);
-    
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider('snoopy-view-scm', provider)
+    );
+
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeTextDocument(() => {
+            provider.handleType();
+        })
+    );
+
     vscode.window.onDidEndTerminalShellExecution(event => {
-        if (event.exitCode !== 0) {
-            if(provider._view) {
-                provider._view.webview.postMessage({ command: 'error'});
+        try {
+            const cmd = event.execution.commandLine.value || "";
+            if (event.exitCode !== 0) {
+                provider.sendMessage({ command: 'error' });
+            } else if (event.exitCode === 0 && (cmd.includes('push') || cmd.includes('commit'))) {
+                provider.sendMessage({ command: 'success' });
             }
-        }
+        } catch (e) {}
     });
+
+    try {
+        const gitExtension = vscode.extensions.getExtension('vscode.git');
+        if (gitExtension) {
+            await gitExtension.activate();
+            const gitApi = gitExtension.exports.getAPI(1);
+
+            gitApi.repositories.forEach(repo => {
+                let lastCommit = repo.state.HEAD?.commit;
+
+                repo.state.onDidChange(() => {
+                    const currentCommit = repo.state.HEAD?.commit;
+                    if (currentCommit !== lastCommit) {
+                        lastCommit = currentCommit;
+                        provider.sendMessage({ command: 'success' });
+                    }
+                });
+            });
+
+            gitApi.onDidOpenRepository(repo => {
+                let lastCommit = repo.state.HEAD?.commit;
+                repo.state.onDidChange(() => {
+                    const currentCommit = repo.state.HEAD?.commit;
+                    if (currentCommit !== lastCommit) {
+                        lastCommit = currentCommit;
+                        provider.sendMessage({ command: 'success' });
+                    }
+                });
+            });
+        }
+    } catch (error) {}
 }
 
 function deactivate() {}
-module.exports = { 
+
+module.exports = {
     activate,
     deactivate
 };
-
